7a009dd8d10402b0865eeba84235838d59cea111
Steve <stg7@gmx.de>
2023-02-21 13:10:19 +0100

    update download script

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 base_url="https://avtshare01.rz.tu-ilmenau.de/pnats-uhd-1-long"
 
-wget --no-parent -r -np -nH --cut-dirs=2 -m -R "*.html*" -P src_videos "$base_url"/src_videos/
-#wget --no-parent -r -np -nH --cut-dirs=2 -m -R "*.html*" -P cpvs "$base_url"/cpvs/
+wget --no-parent -r -np -nH --cut-dirs=2 -m -R "*.html*" -P srcs "$base_url"/srcs/
+wget --no-parent -r -np -nH --cut-dirs=2 -m -R "*.html*" -P avpvs "$base_url"/avpvs/
